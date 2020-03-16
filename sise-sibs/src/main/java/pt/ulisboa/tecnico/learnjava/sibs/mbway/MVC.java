f5db464efce168ab455440488eca84fefea4b3c3
André Santos <andre.santos.95@hotmail.com>
2020-03-16 20:41:37 +0000

    v3.1

--- sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbway/MVC.java
+++ sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbway/MVC.java
@@ -5,53 +5,42 @@ import java.util.Scanner;
 
 public class MVC {
 	private static Scanner SCANNER = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
+		Controller controller = new Controller();
 
-	      //fetch student record based on his roll no from the database
-	      MBWayAccount model  = retriveMBWayAccount();
+		while (true) {
+			System.out.println("Insert command: ");
+			String input = SCANNER.nextLine();
+			String[] inputs = input.split(" ");
 
-	      //Create a view : to write student details on console
-	      View view = new View();
 
-	      Controller controller = new Controller();
+			if(input.equals("exit")) { 
+				System.out.println("You exit the app.");
+				break; 
 
-	      while (true) {
-	    	  System.out.println("Insert command: ");
-	    	  String input = SCANNER.nextLine();
-	    	  String[] inputs = input.split(" ");
-	    	  
-	    	  
-	    	  if(input.equals("exit")) { 
-	    		  break; 
-	    		  
-	    	  } else if (inputs[0].contentEquals("associate-mbway")) {
-	    		  controller.createMBWay(inputs[1], Integer.parseInt(inputs[2]));
-	    		  
-	    	  } else if (inputs[0].contentEquals("confirm-mbway")) {
-	    		  controller.confirmAccount(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
-	    		  
-	    	  } else if (inputs[0].contentEquals("mbway-transfer")) {
-	    		  // mbway-transfer <SOURCE_PHONE_NUMBER> <TARGET_PHONE_NUMBER> <AMOUNT>
-	    		  controller.transfer(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]), Integer.parseInt(inputs[3]));
-	    		  
-	    	  } else if (inputs[0].contentEquals("mbway-split-bill")) {
-	    		  // mbway-split-bill <NUMBER_OF_FRIENDS> <AMOUNT>
-	    		  
-	    		  int counter = 0;
-	    		  HashMap<Integer, Integer> splits = new HashMap<Integer, Integer>();
-	    		  for (int i = 0; i < Integer.parseInt(inputs[1]); i++) {
-	    			  // friend <PHONE_NUMBER> <AMOUNT>
-	    			  
-	    		  }
-	    		  
-	    	  }  
-	      }
-	   }
+			} else if (inputs[0].contentEquals("associate-mbway")) {
+				controller.createMBWay(inputs[1], Integer.parseInt(inputs[2]));
 
-	   private static MBWayAccount retriveMBWayAccount(){
-	      MBWayAccount mbAccount = new MBWayAccount("BPICK1", 911111111);
-	      return mbAccount;
-	   }
-	   
+			} else if (inputs[0].contentEquals("confirm-mbway")) {
+				controller.confirmAccount(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+
+			} else if (inputs[0].contentEquals("mbway-transfer")) {
+				// mbway-transfer <SOURCE_PHONE_NUMBER> <TARGET_PHONE_NUMBER> <AMOUNT>
+				controller.transfer(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]), Integer.parseInt(inputs[3]));
+
+			} else if (inputs[0].contentEquals("mbway-split-bill")) {
+				// mbway-split-bill <NUMBER_OF_FRIENDS> <AMOUNT>
+
+				int counter = 0;
+				HashMap<Integer, Integer> splits = new HashMap<Integer, Integer>();
+				for (int i = 0; i < Integer.parseInt(inputs[1]); i++) {
+					// friend <PHONE_NUMBER> <AMOUNT>
+
+				}
+
+			}  
+		}
+	}
+
 }
